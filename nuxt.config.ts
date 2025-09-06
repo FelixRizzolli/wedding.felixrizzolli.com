644ab140774acb2fa4b336be33eea56c522998b7
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-09-06 17:07:47 +0000

    feat: install nuxt-svgo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,29 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'nuxt-svgo'],
+
+  svgo: {
+    componentPrefix: 'i',
+    autoImportPath: "~/assets/images/",
+    customComponent: "MyIcon",
+    svgoConfig: {
+      // multipass: true, // optimization will be applied until the result differs from the one obtained in the previous step
+      plugins: [
+          {
+              name: "preset-default", //  default set of plugins, https://svgo.dev/docs/preset-default/#plugins-list
+              params: {
+                  overrides: {
+                      removeViewBox: false, // viewbox is required for svg to scale properly
+                      mergePaths: false, // disable mergePaths to preserve all paths
+                      collapseGroups: false, // disable collapseGroups to preserve groups
+                  },
+              },
+          },
+          // additional plugins, https://svgo.dev/docs/plugins/
+          "removeDimensions", // remove width and height attributes,
+          "removeXMLNS", // remove xmlns attribute
+      ],
+    },
+  },
 })
